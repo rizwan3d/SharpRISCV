7a2b6bbc5aa09bf5c9bf433939c1d6eac3161b07
Muhammad Rizwan <allgame45@hotmail.com>
2023-10-16 11:36:13 +0300

    Complete: ELF Dump Highlight

---
+++ WebRISCV/CircularIdGenerator.cs
@@ -0,0 +1,26 @@
+﻿namespace WebRISCV
+{
+    public class CircularIdGenerator
+    {
+        private string[] colors;
+        private int currentIndex;
+
+        public CircularIdGenerator(string[] inputColors)
+        {
+            colors = inputColors;
+            currentIndex = 0;
+        }
+
+        public string Current 
+        {
+            get { return colors[currentIndex]; }
+        }
+
+        public string GetNextId()
+        {
+            string nextId = colors[currentIndex];
+            currentIndex = (currentIndex + 1) % colors.Length;
+            return nextId;
+        }
+    }
+}
