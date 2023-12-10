4b6705af232a7c8067d88b73554e0dddf74bb54e
Muhammad Rizwan <allgame45@hotmail.com>
2023-12-10 16:20:00 +0300

    Add: GetBytes in Data Class

--- SharpRISCV.Core.V2/Program/Datas/Data.cs
+++ SharpRISCV.Core.V2/Program/Datas/Data.cs
@@ -1,10 +1,14 @@
 ﻿using SharpRISCV.Core.V2.Directive;
 using SharpRISCV.Core.V2.LexicalToken.Abstraction;
 using SharpRISCV.Core.V2.Program.Datas.Abstraction;
+using SharpRISCV.Core.V2.Program.Sections;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text;
+using System.Collections;
 
 namespace SharpRISCV.Core.V2.Program.Datas
 {
-    class Data(IToken Type) : DataBase(Type), IData
+    public class Data(IToken Type) : DataBase(Type), IData
     {
         private int? IntData { get; set; }
         private string StringData { get; set; } = string.Empty;
@@ -12,6 +16,39 @@ namespace SharpRISCV.Core.V2.Program.Datas
         public override bool IsComplete()
         {
             return !string.IsNullOrEmpty(StringData) || IntData is not null;
+        }
+
+        public object GetData()
+        {
+            return IntData is null? StringData: IntData;
+        }
+
+        public List<byte> GetBytes()
+        {
+            List<byte> bytes = [];
+            if (Directives.IsString(Type))
+            {
+                byte[] byteArray = new UTF8Encoding(true).GetBytes(StringData);
+                bytes.AddRange(byteArray);
+                bytes.Add(0);
+            }
+            else if (Directives.IsWord(Type))
+            {
+                byte[] byteArray = BitConverter.GetBytes(IntData.GetValueOrDefault());
+                if (byteArray.Length < 4)
+                {
+                    byte[] paddedArray = new byte[4];
+                    Array.Copy(byteArray, paddedArray, byteArray.Length); // Copy the original bytes
+                    byteArray = paddedArray; // Replace byteArray with the padded array
+                }
+                bytes.AddRange(byteArray);
+            }
+            else if (Directives.IsSpace(Type))
+            {
+                bytes.AddRange(new byte[IntData.GetValueOrDefault()]);
+            }
+
+            return bytes;
         }
 
         public override void SetData(IToken token)
