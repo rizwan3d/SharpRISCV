d89bace6c581ab1173d56c674da2b5c1743f7e94
Muhammad Rizwan <allgame45@hotmail.com>
2023-12-12 11:42:29 +0300

    Add: Partial Semantic Analysis

---
+++ SharpRISCV.Core.V2/SemanticAnalysis/Abstraction/IAnalyzer.cs
@@ -0,0 +1,13 @@
+﻿using SharpRISCV.Core.V2.FirstPass.Abstraction;
+using SharpRISCV.Core.V2.LexicalToken.Abstraction;
+using SharpRISCV.Core.V2.Program.Datas.Abstraction;
+using SharpRISCV.Core.V2.Program.Instructions.Abstraction;
+using SharpRISCV.Core.V2.Program.Sections.Abstraction;
+
+namespace SharpRISCV.Core.V2.ParseTree.Abstraction
+{
+    public interface IAnalyzer
+    {
+        void Analyze(IInstruction Instruction, ISymbolTable symbolTable);
+    }
+}
---
+++ SharpRISCV.Core.V2/SemanticAnalysis/SemanticAnalysis.cs
@@ -0,0 +1,21 @@
+﻿using SharpRISCV.Core.V2.Program.Sections.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace SharpRISCV.Core.V2.SemanticAnalysis
+{
+    public class SemanticAnalysis(List<ISection> sections)
+    {
+        public void Perform()
+        {
+            foreach (ISection section in sections.Where( s => s is ITextSection))
+            {
+
+            }
+        }
+    }
+}
