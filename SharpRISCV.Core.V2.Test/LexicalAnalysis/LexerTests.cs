a66903b7d96c543d76c742858104343c50edd806
Muhammad Rizwan <allgame45@hotmail.com>
2023-12-13 12:11:49 +0300

    Add: Unit test for new pattern

--- SharpRISCV.Core.V2.Test/LexicalAnalysis/LexerTests.cs
+++ SharpRISCV.Core.V2.Test/LexicalAnalysis/LexerTests.cs
@@ -183,5 +183,51 @@ namespace SharpRISCV.Core.V2.Test.LexicalAnalysis
 
             Assert.AreEqual(TokenType.EPSILONE, tokens.TokenType);
         }
+
+        [TestMethod]
+        public void Lexer_Tokenize_Complex_MultiLine_Success()
+        {
+            string assemblyCode = @".text                            # code segment
+                                        addi  sp,  sp, -16           # reserve stack space
+                                        sw    ra,  12(sp)            # save return address
+                                    _start:                          # program entry
+                                        lui   a0,  %hi(.LC0)         # load message address
+                                        addi  a0,  a0, %lo(.LC0  )     # message address
+                                        la    x10, .LC0              # test la: load address
+                                        lui   a5,  %hi(  w_addr)       # test %hi() address
+                                        lw    a5,  %lo(w_addr)(a5)   # test lw %lo() address
+                                        lw    ra,  12(sp)            # restore return address
+                                        addi  sp,  sp, 16            # release stack space
+                                    .data                            # data segment
+                                    w_addr:                          # word address
+                                        .word 1495296332             # 1 word
+                                        .word 1852403041             # 1 word
+                                        .word 0,0                    # 2 wordsd
+                                    .LC0: 
+                                        .string ""Hello, World!\n""    # a string
+                                    .end
+                                    ";
+
+            Lexer lexer = new Lexer(assemblyCode);
+            var tokens = lexer.Tokenize().ToList();
+
+            Assert.IsNotNull(tokens);
+            Assert.AreEqual(47, tokens.Count);
+
+            Assert.AreEqual(TokenType.INTEGER, tokens[4].TokenType);
+            Assert.AreEqual("-16", tokens[4].Value);
+
+            Assert.AreEqual(TokenType.LABEL, tokens[12].TokenType);
+            Assert.AreEqual("%hi(.LC0)", tokens[12].Value);
+
+            Assert.AreEqual(TokenType.LABEL, tokens[25].TokenType);
+            Assert.AreEqual("%lo(w_addr)(a5)", tokens[25].Value);
+
+            Assert.AreEqual(TokenType.INTEGER, tokens[28].TokenType);
+            Assert.AreEqual("12", tokens[28].Value);
+
+            Assert.AreEqual(TokenType.REGISTER, tokens[29].TokenType);
+            Assert.AreEqual("sp", tokens[29].Value);
+        }
     }
 }
